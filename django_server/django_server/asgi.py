aaec2c71af74380e00cc45a8f79a97abe7dd7265
julian <j.schoepe@ostfalia.de>
2025-05-07 15:18:02 +0200

    remove allow origin validator

--- django_server/django_server/asgi.py
+++ django_server/django_server/asgi.py
@@ -2,7 +2,6 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 
@@ -15,6 +14,6 @@ from farminsight_dashboard_backend.routing import websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
