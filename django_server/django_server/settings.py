4cc6ecdc03e6ca85da7eb527e2ae16d638abb2e2
Julian Schöpe <j.schoepe@ostfalia.de>
2025-05-09 13:29:13 +0200

    also correct exception handler path in settings

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -239,7 +239,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ),
-    'EXCEPTION_HANDLER': 'farminsight_dashboard_backend.exceptions.exceptions.custom_exception_handler'
+    'EXCEPTION_HANDLER': 'farminsight_dashboard_backend.exceptions.custom_exception_handler'
 }
 
 # 0 or negative for indefinite duration
