3421e58c749827e3dd25594a0f327948da59a30a
js-ostfalia <165173224+js-ostfalia@users.noreply.github.com>
2025-12-05 12:24:24 +0100

    back to less logging

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -165,7 +165,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
         'farminsight_dashboard_backend': {
