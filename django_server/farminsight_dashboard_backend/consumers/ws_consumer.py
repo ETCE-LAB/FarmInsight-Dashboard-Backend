ba3f3b8796da78753ca4105fdbe17b829295618e
Tom <t.heering@ostfalia.de>
2025-12-10 09:59:45 +0100

    Emergency BugFix: Debug Logging removed from ws_consumer.py

--- django_server/farminsight_dashboard_backend/consumers/ws_consumer.py
+++ django_server/farminsight_dashboard_backend/consumers/ws_consumer.py
@@ -55,7 +55,6 @@ class CameraLivestreamConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         try:
-            logger.info("Client connected to CameraLivestreamConsumer")
             self.room_name = self.scope['url_route']['kwargs']['camera_id']
             self.room_group_name = f'camera_livestream_{self.room_name}'
             logger.info(self.room_group_name)
@@ -67,9 +66,11 @@ class CameraLivestreamConsumer(AsyncWebsocketConsumer):
             try:
                 camera = await sync_to_async(get_active_camera_by_id)(self.room_name)
                 livestream_url = camera.livestreamUrl
-            except Exception:
-                logger.info("Client failed to connect to CameraLivestreamConsumer")
-                return
+            except Exception as e:
+                await LogMessage.objects.acreate(
+                    message=f"{e}",
+                    logLevel='INFO',
+                )
 
             # Start of streaming task (if not already started)
             await WebsocketStreamingManager.add_client(self.room_name, livestream_url, self.room_group_name)
@@ -108,7 +109,6 @@ class WebsocketStreamingManager:
 
     @classmethod
     async def add_client(cls, camera_id: str, livestream_url: str, group_name: str, max_fps: int = 5):
-        logger.info(f'New Client for Camera Livestream: {camera_id} ' )
         async with cls._lock:
 
             entry = cls._streams.get(camera_id)
