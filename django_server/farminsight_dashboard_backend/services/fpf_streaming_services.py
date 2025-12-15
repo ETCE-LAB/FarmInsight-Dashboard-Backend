0af5b094ce3ba396c478752018742001b9f27c1d
julian <j.schoepe@ostfalia.de>
2025-12-15 14:12:44 +0100

    reset logging and framerate

--- django_server/farminsight_dashboard_backend/services/fpf_streaming_services.py
+++ django_server/farminsight_dashboard_backend/services/fpf_streaming_services.py
@@ -18,7 +18,7 @@ async def websocket_stream(livestream_url: str,
     """
     loop = asyncio.get_event_loop()
     channel_layer = get_channel_layer()
-    frame_interval = 1.0 / 20.0
+    frame_interval = 1.0 / 2
 
     # open VideoCapture in the Executor
     cap = await loop.run_in_executor(None, cv2.VideoCapture, livestream_url)
@@ -73,7 +73,7 @@ async def websocket_stream(livestream_url: str,
                     groupname,
                     {"type": "camera_frame", "frame_data": b64}
                 )
-                logger.info("Livestream frame sent")
+                #logger.info("Livestream frame sent")
             last_time = now
 
     finally:
