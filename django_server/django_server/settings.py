f55999e53ce89b18d987a7e54ed23698a3f32e72
Niklas15010 <149146711+Niklas15010@users.noreply.github.com>
2026-01-08 19:48:15 +0100

    Change SQLite database name from db2.sqlite3 to db.sqlite3

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -119,7 +119,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db2.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
