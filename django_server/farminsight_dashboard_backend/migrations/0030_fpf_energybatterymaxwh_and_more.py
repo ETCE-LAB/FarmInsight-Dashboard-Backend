2eb4cf50bf055953b0d7b4f609a7ea47a50fcfc4
M <max.linke@Ostfalia.de>
2026-01-28 14:59:08 +0100

    Fixed issues for local env

--- django_server/farminsight_dashboard_backend/migrations/0030_fpf_energybatterymaxwh_and_more.py
+++ django_server/farminsight_dashboard_backend/migrations/0030_fpf_energybatterymaxwh_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('farminsight_dashboard_backend', '0029_fpf_isactive'),
+        ('farminsight_dashboard_backend', '0029_alter_resourcemanagementmodel_options_fpf_isactive_and_more'),
     ]
 
     operations = [
